73bf7e9ed223b89c26053fbe8929993254c92a69
Marcelo Oliveira <marcelotk_15@hotmail.com>
2021-01-12 13:41:15 -0300

    📦 NEW: routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
+const {cepFormat} = require('./middleware')
+const CepController = require('./Controllers/CEPController')
+
 const router = express.Router();
 
 // Home... Wello world
 router.get('/', (req, res) => {
   res.json({ status: 200, msg: 'Olá mundo' })
 })
+
+router.get('/cep/:cep', cepFormat, CepController.searchByCep)
+router.get('/rua/:street', CepController.searchByStreet)
 
 module.exports = router
